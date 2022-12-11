ba3cbe94e397809a031f84b7e2d223cf22aa597b
lateeeef <olalekanoluwa2@gmail.com>
2022-12-11 15:55:57 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry
  * Return: Always 0
- * */
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzlee);
+	return (0);
 }
