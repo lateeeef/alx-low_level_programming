97466261ff870de5997a659e9b722c214ce1b9eb
lateeeef <olalekanoluwa2@gmail.com>
2022-12-11 16:24:54 -0800

    Advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Return: Always 1
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpor, 2015-10-19\n", 59);
+	return (1);
+}
