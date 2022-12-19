cc4ef6ebe0ac96eb4b11fe637b49a38d5b26bbec
lateeeef <olalekanoluwa2@gmail.com>
2022-12-19 15:50:43 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 function that take a pointer to an integer parametr
+ * @n: integer pointer
+ * Return: 0 Success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
